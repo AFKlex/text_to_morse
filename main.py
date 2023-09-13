224be706b12e0490b7f606bb548f4b3a87ff4ab8
Alexander Agricola <agricolaalex@gmail.com>
2023-09-13 14:38:45 +0200

    Add functionality for creation of a morse code file mp3 file

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ clear_input = input("Bitte geben Sie hier ihren Text ein:")
 
 morse_string = convert_string_to_morse(clear_input)
 sound.play_sound(morse_string)
+
+sound.create_mp3(morse_string)
--- sound.py
+++ sound.py
@@ -1,5 +1,7 @@
 from pygame import mixer 
 import time
+from pydub import AudioSegment
+
 
 mixer.init()
 short_beep =mixer.Sound("assets/morse_short.mp3")
@@ -15,3 +17,18 @@ def play_sound(morse_string:str):
             time.sleep(0.5)
         else:
             time.sleep(1)
+
+
+def create_mp3(morse_string:str):
+    combined_audio = AudioSegment.empty()
+    for beep in morse_string:
+        if beep == "-":
+            combined_audio += AudioSegment.from_file("assets/morse_short.mp3")
+            combined_audio += AudioSegment.silent(duration=500)
+        elif beep == ".":
+            combined_audio += AudioSegment.from_file("assets/morse_long.mp3")
+            combined_audio += AudioSegment.silent(duration=500)
+        else:
+            combined_audio += AudioSegment.silent(duration=1000)
+
+    combined_audio.export("output_audio.mp3", format="mp3")
